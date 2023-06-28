7bc92340b9fad06d78755419bf34f507460920c4
Dan Levi Obello <danleviobello@Dans-MBP.attlocal.net>
2023-06-28 18:06:42 -0500

    Added some more questions for the game

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -7,7 +7,25 @@ var quizQuestions = [ {
         d: "Python",
     },
     {
-        question: ""
+        question: "Commonly used types do NOT include",
+        a: "Strings",
+        b: "Booleans",
+        c: "Arrays",
+        d: "Alerts",
+    },
+    {
+        question: "Arrays in Javascript can contain",
+        a: "Booleans",
+        b: "Other arrays",
+        c: "Events",
+        d: "All of the above",
+    },
+    {
+        quesiton: "Which naming convention is used more often in JavaScript?",
+        a: "hyp-hens",
+        b: "under_scores",
+        c: "camelCase",
+        d: "PascalCase",
     }
 ] 
 
