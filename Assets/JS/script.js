2e079959272b74001140ac6d0010d9286f62a3f6
Dan Levi Obello <danleviobello@Dans-MBP.attlocal.net>
2023-06-28 17:21:22 -0500

    Worked on the HTML Base

---
+++ Assets/JS/script.js
@@ -0,0 +1,13 @@
+// Questions to be asked
+var quizQuestions = [ {
+        question: "Which Language runs in a web browser?",
+        a: "Java",
+        b: "C#",
+        c: "JavaScript",
+        d: "Python",
+    },
+    {
+        question: ""
+    }
+] 
+
